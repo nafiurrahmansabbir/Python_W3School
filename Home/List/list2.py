136fee9527f2b089722c664e00ea199c0a6540ea
sabbir <nafiurrahmansabbir@gmail.com>
2024-03-09 01:51:53 +0600

    added something

---
+++ Home/List/list2.py
@@ -0,0 +1,21 @@
+thislist = ["apple", "banana", "cherry"]
+list2=[]
+# for i in range(len(thislist)):
+#   list2.append(thislist[i])
+# #   print(thislist[i])
+for i in thislist:
+  list2.append(i)
+#   print(thislist[i])
+
+
+
+print(list2)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+print(fruits)
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
---
+++ Home/Tuple/tuple2.py
@@ -0,0 +1,13 @@
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
